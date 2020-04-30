a849d612148af47e6847673f8662ec576b92005c
justinr922 <justinr922@gmail.com>
2020-04-30 00:00:44 -0400

    Update profile Functionality

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,6 +9,11 @@ class Profile(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args,**kwargs)
-        
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
