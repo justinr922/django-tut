c37640c67d90931a4e696099578e65efad54a84e
justinr922 <justinr922@gmail.com>
2020-05-02 21:33:24 -0400

    Added pagination

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Home Page / About
     path('', views.PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    # Post pages
     path('post/<int:pk>/', views.PostDetailView.as_view(), name = 'post-detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name = 'post-update'),
     path('post/new/', views.PostCreateView.as_view(), name = 'post-create'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post-delete')
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post-delete'),
+    path('user/<str:username>/', views.UserPostListView.as_view(), name = 'user-posts'),
 ]
