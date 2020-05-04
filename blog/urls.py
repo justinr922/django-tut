3fcde14422e9fbc6e22fd83904bb8a44fcfb85c0
justinr922 <justinr922@gmail.com>
2020-05-03 21:15:16 -0400

    Fixed Update Post Button

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('about/', views.about, name='blog-about'),
     # Post pages
     path('post/<int:pk>/', views.PostDetailView.as_view(), name = 'post-detail'),
-    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name = 'post-update'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(template_name = 'blog/post_update.html'), name = 'post-update'),
     path('post/new/', views.PostCreateView.as_view(), name = 'post-create'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name = 'post-delete'),
     path('user/<str:username>/', views.UserPostListView.as_view(), name = 'user-posts'),
